c17a297e25a3794c5121f2094dddd817d0a2cfb6
Emily Morgan <emorgan05@gmail.com>
2017-09-29 20:48:49 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,18 +15,18 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count
-    self.each do |artist|
-      @@song_count += artist.songs.length
-    end
+    @@song_count
   end
 
 end
